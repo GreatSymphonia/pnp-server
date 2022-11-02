cdf78129adbde9343d7419318a23abe739f3b274
th.l <thl-cmk@outlook.com>
2022-11-02 17:52:32 +0100

    Code optimized --> moved print into log function, startet with fStrings, split software_images struckture

--- ztp.py
+++ ztp.py
@@ -2,119 +2,135 @@
 from cli import configure, cli, configurep, executep
 import re
 import time
-import urllib
+# import urllib
 import sys
 import logging
-import os
+# import os
 from logging.handlers import RotatingFileHandler
-import subprocess 
+# import subprocess
 
-software_mappings = {
-    'C9300-24P': {
+
+software_images = {
+    'CAT9K': {
         'software_image': 'cat9k_iosxe.17.06.01.SPA.bin',
         'software_version': '17.06.01',
-        'software_md5_checksum': 'fdb9c92bae37f9130d0ee6761afe2919'
+        'software_md5_checksum': 'fdb9c92bae37f9130d0ee6761afe2919',
     },
-    'C9500-24Q': {
-        'software_image': 'cat9k_iosxe.17.06.01.SPA.bin',
-        'software_version': '17.06.01',
-        'software_md5_checksum': 'fdb9c92bae37f9130d0ee6761afe2919'
-    },
-    'ASR1001-HX': {
+    'ASR1000': {
         'software_image': 'asr1000-universalk9.17.05.01a.SPA.bin',
         'software_version': '17.05.01a',
-        'software_md5_checksum': '0e4b1fc1448f8ee289634a41f75dc215'
+        'software_md5_checksum': '0e4b1fc1448f8ee289634a41f75dc215',
     },
-        'C1117-4PMLTEEAWE': {
+    'C1100': {
         'software_image': 'c1100-universalk9.17.06.03a.SPA.bin',
         'software_version': '17.06.03a',
-        'software_md5_checksum': '2501b21b6fa3f71ea6acd7d59bcc8423'    
+        'software_md5_checksum': '2501b21b6fa3f71ea6acd7d59bcc8423',
+    },
+}
+
+models = {
+    'C9300-24P': {
+        'software_image': 'CAT9K',
+    },
+    'C9500-24Q': {
+        'software_image': 'CAT9K',
+    },
+    'ASR1001-HX': {
+        'software_image': 'ASR1000',
+    },
+    'C1117-4PMLTEEANE': {
+        'software_image': 'C1100',
     }
 }
+
 
 http_server = '192.168.10.15'
 log_tofile = True
 release_set = ['16.06', '16.07']
 
+
 def main():
-    
     try:
         # switch to enable/disbale persistent logger
-        if(log_tofile == True):
+        if log_tofile:
             filepath = create_logfile()
             configure_logger(filepath)
-      
+
         log_info('###### STARTING ZTP SCRIPT ######\n')
         log_info('**** Determining Device Model ****\n')
         model = get_model()
-        
-        software_image = software_mappings[model]['software_image']
-        software_version = software_mappings[model]['software_version']
-        software_md5_checksum = software_mappings[model]['software_md5_checksum']
-        
+
+        software_image = software_images[models[model]['software_image']]
+        software_version = software_images[models[model]['software_version']]
+        software_md5_checksum = software_images[models[model]['software_md5_checksum']]
+
         log_info('**** Checking if upgrade is required or not ***** \n')
         update_status, current_version = upgrade_required(software_version)
         if update_status:
-            #check if image transfer needed
+            # check if image transfer needed
             if check_file_exists(software_image):
-              print(current_version)
-              if current_version[0:5] not in release_set:
-                if not verify_dst_image_md5(software_image, software_md5_checksum):
-                    log_info('*** Attempting to transfer image to switch.. ***')
-                    file_transfer(http_server, software_image)
+                print(current_version)
+                if current_version[0:5] not in release_set:
                     if not verify_dst_image_md5(software_image, software_md5_checksum):
-                        log_critical('*** Failed Xfer mds hash mismatch ***')
-                        raise ValueError('Failed Xfer')
+                        log_info('*** Attempting to transfer image to switch.. ***')
+                        file_transfer(http_server, software_image)
+                        if not verify_dst_image_md5(software_image, software_md5_checksum):
+                            log_critical('*** Failed Xfer mds hash mismatch ***')
+                            raise ValueError('Failed Xfer')
             else:
-              file_transfer(http_server, software_image)
-              if current_version[0:5] not in release_set:
-                if not verify_dst_image_md5(software_image, software_md5_checksum):
-                    log_critical('XXX Failed Xfer XXX')
-                    raise ValueError('XXX Failed Xfer XXX')
-            
+                file_transfer(http_server, software_image)
+                if current_version[0:5] not in release_set:
+                    if not verify_dst_image_md5(software_image, software_md5_checksum):
+                        log_critical('XXX Failed Xfer XXX')
+                        raise ValueError('XXX Failed Xfer XXX')
+
             log_info('*** Deploying EEM upgrade script ***')
             deploy_eem_upgrade_script(software_image)
             log_info('*** Performing the upgrade - switch will reboot ***\n')
             cli('event manager run upgrade')
             time.sleep(600)
-            log_info('*** EEM upgrade took more than 600 seconds to reload the device..Increase the sleep time by few minutes before retrying  ***\n')
+            log_info(
+                '*** EEM upgrade took more than 600 seconds to reload the device. Increase the sleep time by few minutes before retrying  ***\n')
         else:
-          log_info('*** No upgrade is required!!! *** \n')
+            log_info('*** No upgrade is required!!! *** \n')
 
-    # Cleanup any leftover install files
+        # Cleanup any leftover install files
         log_info('*** Deploying Cleanup EEM Script ***')
         deploy_eem_cleanup_script()
         log_info('*** Running Cleanup EEM Script ***')
         cli('event manager run cleanup')
         time.sleep(30)
 
-    #print config file name to download
+        # print config file name to download
         config_file = '%s.cfg' % model
         log_info('**** Downloading config file ****\n')
         file_transfer(http_server, config_file)
         log_info('*** Trying to perform  Day 0 configuration push  **** \n')
-        #configure_replace(config_file)
+        # configure_replace(config_file)
         configure_merge(config_file)
         configure('crypto key generate rsa modulus 4096')
         log_info('######  END OF ZTP SCRIPT ######\n')
-    
+
     except Exception as e:
-        log_critical('*** Failure encountered during day 0 provisioning . Aborting ZTP script execution. Error details below   ***\n')
+        log_critical(
+            '*** Failure encountered during day 0 provisioning. Aborting ZTP script execution. Error details below   ***\n')
         log_critical(e)
         sys.exit(e)
-       
 
-def configure_replace(file,file_system='flash:/' ):
-        config_command = 'configure replace %s%s force' % (file_system, file)
-        log_info('************************Replacing configuration************************\n')
-        config_repl = executep(config_command)
-        time.sleep(120)
-    
-def configure_merge(file,file_system='flash:/'):
-     log_info('************************Merging running config with given config file************************\n')
-     config_command = 'copy %s%s running-config' %(file_system,file)
-     config_repl = executep(config_command)
-     time.sleep(120)
+
+def configure_replace(file, file_system='flash:/'):
+    config_command = f'configure replace {file_system}{file} force'
+    log_info('************************Replacing configuration************************\n')
+    config_repl = executep(config_command)
+    time.sleep(120)
+
+
+def configure_merge(file, file_system='flash:/'):
+    log_info('************************Merging running config with given config file************************\n')
+    config_command = f'copy {file_system}{file} running-config'
+    config_repl = executep(config_command)
+    time.sleep(120)
+
 
 def check_file_exists(file, file_system='flash:/'):
     dir_check = 'dir ' + file_system + file
@@ -132,37 +148,42 @@ def check_file_exists(file, file_system='flash:/'):
     else:
         log_critical('************************Unexpected output from check_file_exists************************\n')
         raise ValueError("Unexpected output from check_file_exists")
-         
+
 
 def deploy_eem_cleanup_script():
     install_command = 'install remove inactive'
-    eem_commands = ['event manager applet cleanup',
-                    'event none maxrun 600',
-                    'action 1.0 cli command "enable"',
-                    'action 2.0 cli command "%s" pattern "\[y\/n\]"' % install_command,
-                    'action 2.1 cli command "y" pattern "proceed"',
-                    'action 2.2 cli command "y"'
-                    ]
+    eem_commands = [
+        f'event manager applet cleanup',
+        f'event none maxrun 600',
+        f'action 1.0 cli command "enable"',
+        f'action 2.0 cli command "{install_command}" pattern "\[y\/n\]"'
+        f'action 2.1 cli command "y" pattern "proceed"',
+        f'action 2.2 cli command "y"'
+    ]
     results = configurep(eem_commands)
     log_info('*** Successfully configured cleanup EEM script on device! ***')
 
+
 def deploy_eem_upgrade_script(image):
-    install_command = 'install add file flash://' + image + ' activate commit'
-    eem_commands = ['event manager applet upgrade',
-                    'event none maxrun 600',
-                    'action 1.0 cli command "enable"',
-                    'action 2.0 cli command "%s" pattern "\[y\/n\/q\]"' % install_command,
-                    'action 2.1 cli command "n" pattern "proceed"',
-                    'action 2.2 cli command "y"'
-                    ]
+    install_command = f'install add file flash://{image} activate commit'
+    eem_commands = [
+        f'event manager applet upgrade',
+        f'event none maxrun 600',
+        f'action 1.0 cli command "enable"',
+        f'action 2.0 cli command "{install_command}" pattern "\[y\/n\/q\]"' 
+        f'action 2.1 cli command "n" pattern "proceed"',
+        f'action 2.2 cli command "y"'
+    ]
     results = configurep(eem_commands)
     log_info('*** Successfully configured upgrade EEM script on device! ***')
 
+
 def file_transfer(http_server, file):
-  log_info('**** Start transferring  file *******\n')
-  res = cli('copy http://%s/%s flash:%s' % (http_server,file,file))
-  log_info(res + '\n')
-  log_info('**** Finished transferring device configuration file *******\n')
+    log_info('**** Start transferring  file *******\n')
+    res = cli('copy http://%s/%s flash:%s' % (http_server, file, file))
+    log_info(res + '\n')
+    log_info('**** Finished transferring device configuration file *******\n')
+
 
 def find_certs():
     certs = cli('show run | include crypto pki')
@@ -172,6 +193,7 @@ def find_certs():
         for cert in certs_split:
             command = 'no %s' % (cert)
             configure(command)
+
 
 def get_serial():
     log_info("******** Trying to  get Serial# *********** ")
@@ -186,6 +208,7 @@ def get_serial():
         serial = re.search(r"Processor board ID\s+(\S+)", show_version).group(1)
     return serial
 
+
 def get_model():
     log_info("******** Trying to  get Model *********** ")
     try:
@@ -194,23 +217,26 @@ def get_model():
         time.sleep(90)
         show_version = cli('show version')
     model = re.search(r"Model Number\s+:\s+(\S+)", show_version)
-    if model != None:
+    if model is not None:
         model = model.group(1)
-    else:     
+    else:
         model = re.search(r"cisco\s(\w+-.*?)\s", show_version)
-        if model != None:
-          model = model.group(1)
+        if model is not None:
+            model = model.group(1)
     return model
+
 
 def get_file_system():
     pass
 
-def update_config(file,file_system='flash:/'):
+
+def update_config(file, file_system='flash:/'):
     update_running_config = 'copy %s%s running-config' % (file_system, file)
     save_to_startup = 'write memory'
     log_info("************************Copying to startup-config************************\n")
     running_config = executep(update_running_config)
     startup_config = executep(save_to_startup)
+
 
 def upgrade_required(target_version):
     # Obtains show version output
@@ -224,6 +250,7 @@ def upgrade_required(target_version):
     else:
         return True, current_version
 
+
 def verify_dst_image_md5(image, src_md5, file_system='flash:/'):
     verify_md5 = 'verify /md5 ' + file_system + image
     log_info('Verifying MD5 for ' + file_system + image)
@@ -231,68 +258,71 @@ def verify_dst_image_md5(image, src_md5, file_system='flash:/'):
     try:
         dst_md5 = cli(verify_md5)
         if src_md5 in dst_md5:
-           log_info('*** MD5 hashes match!! ***\n')
-           return True
+            log_info('*** MD5 hashes match!! ***\n')
+            return True
         else:
-          log_info('**** MD5 don\'t checksum mismatch *****')
-          return False
+            log_info('**** Failed transfer due to MD5 checksum mismatch *****')
+            return False
     except Exception as e:
-       log_info('****  MD5 checksum failed due to an exception  *****')
-       log_info(e)
-       return True
-        #output = subprocess.Popen(['md5sum', '/flash/'+image],stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        #stdout_data, stderr_data = output.communicate()
-        #output.wait() 
-        #outputdata = (stdout_data.decode('utf-8')).split()
-        #md5_returned = outputdata[0]
-        #if src_md5 == md5_returned:
-         #   return True
-        #else:
-         #   return False
-    
+        log_info('****  MD5 checksum failed due to an exception  *****')
+        log_info(e)
+        return True
+        # output = subprocess.Popen(['md5sum', '/flash/'+image],stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        # stdout_data, stderr_data = output.communicate()
+        # output.wait()
+        # outputdata = (stdout_data.decode('utf-8')).split()
+        # md5_returned = outputdata[0]
+        # if src_md5 == md5_returned:
+        #   return True
+        # else:
+        #   return False
+
+
 def create_logfile():
     try:
         print("******** Creating  a persistent log file *********** ")
         path = '/flash/guest-share/ztp.log'
-        #file_exists = os.path.isfile(path)
-        #if(file_exists == False):
-          #print ("******** ztp.log file dont exist .  *********** ")
+        # file_exists = os.path.isfile(path)
+        # if(file_exists == False):
+        # print ("******** ztp.log file dont exist .  *********** ")
         with open(path, 'a+') as fp:
-             pass
+            pass
         return path
     except IOError:
-      print("Couldnt create a log file at guset-share .Trying to use  /flash/ztp.log as an alternate log path")
-      path = '/flash/ztp.log'
-      #file_exists = os.path.isfile(path)
-      #if(file_exists == False):
-      #    print ("******** ztp.log file dont exist .  *********** ")
-      with open(path, 'a+') as fp:
-             pass
-      return path
+        print("Couldnt create a log file at guset-share .Trying to use  /flash/ztp.log as an alternate log path")
+        path = '/flash/ztp.log'
+        # file_exists = os.path.isfile(path)
+        # if(file_exists == False):
+        #    print ("******** ztp.log file dont exist .  *********** ")
+        with open(path, 'a+') as fp:
+            pass
+        return path
     except Exception as e:
-         print("Couldnt create a log file to proceed")
+        print("Couldnt create a log file to proceed")
 
 
-def configure_logger(path):
+def configure_logger(path: str):
     log_formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
     logFile = path
-    #create a new file > 5 mb size
-    log_handler = RotatingFileHandler(logFile, mode='a', maxBytes=5*1024*1024, backupCount=10, encoding=None, delay=0)
+    # create a new file > 5 mb size
+    log_handler = RotatingFileHandler(logFile, mode='a', maxBytes=5 * 1024 * 1024, backupCount=10, encoding=None, delay=0)
     log_handler.setFormatter(log_formatter)
     log_handler.setLevel(logging.INFO)
     ztp_log = logging.getLogger('root')
     ztp_log.setLevel(logging.INFO)
     ztp_log.addHandler(log_handler)
-    
-def log_info(message ):
-    if(log_tofile == True):
-        print(message)
+
+
+def log_info(message):
+    print(message)
+    if log_tofile:
         ztp_log = logging.getLogger('root')
         ztp_log.info(message)
 
-def log_critical(message ):
+
+def log_critical(message):
     print(message)
-    if(log_tofile == True):
+    if log_tofile:
         ztp_log = logging.getLogger('root')
         ztp_log.critical(message)
 
