ff2d42d9b7503c910f8acad28ef956bc742602c1
Thl CMK <thl-cmk@outlook.com>
2023-01-07 20:09:23 +0100

    changed image for c11xx

--- pnp/vars/platforms.py
+++ pnp/vars/platforms.py
@@ -17,9 +17,9 @@ PLATFORMS = {
         image='ASR1000',
     ),
     'C1117-4PMLTEEAWE': Model(
-        image='C1100_16_12',
+        # image='C1100_16_12',
         # image='C1100_17_09',
-        # image='C1100_17_06_04',
+        image='C1100_17_06_04',
     ),
     'C1000-8T-2G-L': Model(
         image='C1000_15_02',
